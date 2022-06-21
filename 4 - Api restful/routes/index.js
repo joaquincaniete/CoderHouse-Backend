f4d59c68fe4ddec7956e7ef711040e57984672ee
joaquincaniete <joaquincaniete@gmail.com>
2022-06-21 19:04:25 -0300

    Commit 4 Corregido

--- 4 - Api restful/routes/index.js
+++ 4 - Api restful/routes/index.js
@@ -13,32 +13,45 @@ router.get('/productos', (req, res)=>{
 router.post('/productos', (req,res)=>{
     const{title, price, thumbnail} = req.body
     let id = productos.length
-    productos.push({title, price, thumbnail, id})
-    res.send(productos[productos.length-1])
+    try{
+        productos.push({title, price, thumbnail, id})
+        res.send(productos[productos.length-1])
+    }
+    catch (error){
+        msj = {error: error.message}
+    }
 })
 router.get('/productos/:id', (req, res)=>{
     let producto = productos.find(prod => prod.id == req.params.id);
     let msj;
     if (producto){
-        console.log(producto);
-        msj = producto;
-    } else {
-        console.log(producto);
-        msj = {error: "producto no encontrado"}
+        try{
+            msj = producto;
+        } catch (error){
+            msj = {error: "ocurrio un error, por favor intente nuevamente mas tarde"}
     }
+    } else{msj = {error: "producto no existe"}
+        
+    }
+
     res.send(msj)
 })
 
 router.put('/productos/:id', (req,res)=>{
     let producto = productos.findIndex((prod) => { return prod.id == req.params.id});
+    const{title, price, thumbnail} = req.body
+    let id = +req.params.id
     let msj;
+
     if (producto != -1){
-        productos[producto]=req.body;
-        msj = {ok: "producto actualizado exitosamente"}
-        
+        try{
+
+            productos[producto]={title, price, thumbnail, id};
+            msj = {ok: "producto actualizado exitosamente"}
+        } catch(error) {
+            msj = {error: "ocurrio un error, por favor intente nuevamente mas tarde"}
+        }        
     } else {
-        
-        
         msj = {error: "producto no encontrado"}
     }
     res.send(msj)
@@ -49,10 +62,15 @@ router.delete('/productos/:id', (req,res)=>{
     let msj;
     console.log(producto);
     if (producto != -1){
-        productos.splice(producto, 1);
-        msj = {ok: "producto eliminado exitosamente"}
+        try{
+
+            productos.splice(producto, 1);
+            msj = {ok: "producto eliminado exitosamente"}
+        } catch(error) {
+            msj = {error: "ocurrio un error, por favor intente nuevamente mas tarde"}
         
-    } else {
+    } }
+    else {
         
         msj = {error: "producto no encontrado"}
     }
