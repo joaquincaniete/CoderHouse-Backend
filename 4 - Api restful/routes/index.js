4f34ef4320e3ed4a410fab6efd6af91e8de6fdbd
joaquincaniete <joaquincaniete@gmail.com>
2022-06-22 19:10:31 -0300

    Commit 4 Resubido

--- 4 - Api restful/routes/index.js
+++ 4 - Api restful/routes/index.js
@@ -1,82 +1,76 @@
-const {Router} = require('express')
-const router = Router()
+const { Router } = require("express");
+const router = Router();
 
-const productos = []
+const productos = [];
 
+router.get("/productos", (req, res) => {
+  res.json(productos);
+});
 
-
-
-router.get('/productos', (req, res)=>{
-    res.json(productos)
-})
-
-router.post('/productos', (req,res)=>{
-    const{title, price, thumbnail} = req.body
-    let id = productos.length
-    try{
-        productos.push({title, price, thumbnail, id})
-        res.send(productos[productos.length-1])
-    }
-    catch (error){
-        msj = {error: error.message}
-    }
-})
-router.get('/productos/:id', (req, res)=>{
-    let producto = productos.find(prod => prod.id == req.params.id);
+router.post("/productos", (req, res) => {
+  const { title, price, thumbnail } = req.body;
+  let id = productos.length;
+  try {
+    productos.push({ title, price, thumbnail, id });
+    res.send(productos[productos.length - 1]);
+  } catch (error) {
+    msj = { error: error.message };
+  }
+});
+router.get("/productos/:id", (req, res) => {
+  try {
+    let producto = productos.find((prod) => prod.id == req.params.id);
     let msj;
-    if (producto){
-        try{
-            msj = producto;
-        } catch (error){
-            msj = {error: "ocurrio un error, por favor intente nuevamente mas tarde"}
-    }
-    } else{msj = {error: "producto no existe"}
-        
-    }
-
-    res.send(msj)
-})
-
-router.put('/productos/:id', (req,res)=>{
-    let producto = productos.findIndex((prod) => { return prod.id == req.params.id});
-    const{title, price, thumbnail} = req.body
-    let id = +req.params.id
-    let msj;
-
-    if (producto != -1){
-        try{
-
-            productos[producto]={title, price, thumbnail, id};
-            msj = {ok: "producto actualizado exitosamente"}
-        } catch(error) {
-            msj = {error: "ocurrio un error, por favor intente nuevamente mas tarde"}
-        }        
+    if (producto) {
+      msj = producto;
     } else {
-        msj = {error: "producto no encontrado"}
+      msj = { error: "producto no existe" };
     }
-    res.send(msj)
-})
 
-router.delete('/productos/:id', (req,res)=>{
-    let producto = productos.findIndex((prod) => { return prod.id == req.params.id});
+    res.send(msj);
+  } catch (error) {
+    msj = { error: "ocurrio un error, por favor intente nuevamente mas tarde" };
+  }
+});
+
+router.put("/productos/:id", (req, res) => {
+  try {
+    let producto = productos.findIndex((prod) => {
+      return prod.id == req.params.id;
+    });
+    const { title, price, thumbnail } = req.body;
+    let id = +req.params.id;
+    let msj;
+
+    if (producto != -1) {
+      productos[producto] = { title, price, thumbnail, id };
+      msj = { ok: "producto actualizado exitosamente" };
+    } else {
+      msj = { error: "producto no encontrado" };
+    }
+    res.send(msj);
+  } catch (error) {
+    msj = { error: "ocurrio un error, por favor intente nuevamente mas tarde" };
+  }
+});
+
+router.delete("/productos/:id", (req, res) => {
+  try {
+    let producto = productos.findIndex((prod) => {
+      return prod.id == req.params.id;
+    });
     let msj;
     console.log(producto);
-    if (producto != -1){
-        try{
-
-            productos.splice(producto, 1);
-            msj = {ok: "producto eliminado exitosamente"}
-        } catch(error) {
-            msj = {error: "ocurrio un error, por favor intente nuevamente mas tarde"}
-        
-    } }
-    else {
-        
-        msj = {error: "producto no encontrado"}
+    if (producto != -1) {
+      productos.splice(producto, 1);
+      msj = { ok: "producto eliminado exitosamente" };
+    } else {
+      msj = { error: "producto no encontrado" };
     }
-    res.send(msj)
-})
+    res.send(msj);
+  } catch (error) {
+    msj = { error: "ocurrio un error, por favor intente nuevamente mas tarde" };
+  }
+});
 
-
-
-module.exports = router
+module.exports = router;
