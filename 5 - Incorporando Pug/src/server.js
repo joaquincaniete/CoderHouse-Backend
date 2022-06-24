9666e97f270bdcd55f6503234672a94c36467b2d
joaquincaniete <joaquincaniete@gmail.com>
2022-06-24 19:46:03 -0300

    Commit 5 Agregando Pug

---
+++ 5 - Incorporando Pug/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes/index');
+const path = require('path');
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//carpeta de plantillas
+app.set('views', path.join(__dirname, './views'));
+// engine 
+app.set('view engine', 'pug');
+// peticiones a raiz
+app.use('/', routes);
+
+//enciendo el server
+app.listen(8080,()=>{
+    console.log("servidor escuchando puerto 8080");
+});
